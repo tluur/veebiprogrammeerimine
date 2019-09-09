945e0623db89b890e054b018ea0859d8806e9fb6
Taavi Luur <tluur@users.noreply.github.com>
2019-09-09 11:20:46 +0300

    õppetöö failistruktuur loomine

---
+++ 02tund/try.php
@@ -0,0 +1,37 @@
+<?php
+  $userName = "Taavi Luur";
+  $fullTimeNow = date("d.m.Y H:i:s"); 
+  $hourNow = date("H");
+  $partOfDay = " hägune aeg";
+  if($hourNow < 8){
+	  $partOfDay = "varane hommik";	  
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+<meta charset="utf-8">
+<title>
+	<?php
+	  echo $userName;	
+	?>
+  esimene leht</title>
+</head>
+<body background="bg.gif">
+<?php
+  echo "<h1>" .$userName ." koolitöö leht</h1>"
+?>
+<p><b>Antud leht on loodud koolis õppetöö raames ja ei sisalda tõsiselt võetavat sisu !</b></p>
+<hr>
+<p>Lehe avamise hetkel oli aeg : 
+<?php
+  echo $fullTimeNow;
+?>
+.</p>
+<?php
+  echo "<p>Leheavanemise hetkel oli" .$partOfDay .
+".</p>"; 
+?>
+</body>
+</html>
